cccbeead4b52fb4d2c3202a95a91907d8e6fe397
KKU89 <krishnakantdeewan@gmail.com>
2026-01-22 23:38:40 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Sum Of Digits/sum-of-digits.java
@@ -0,0 +1,10 @@
+class Solution {
+    static int sumOfDigits(int n) {
+        // code here
+        int sum=0;
+        while(n>0){
+            sum=sum+ n%10;
+            n /=10;
+        }return sum;
+    }
+}
