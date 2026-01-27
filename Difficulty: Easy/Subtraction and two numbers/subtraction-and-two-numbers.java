f9eb99b075391289d69b0032c7c9d31ba19e8032
KKU89 <krishnakantdeewan@gmail.com>
2026-01-27 23:06:16 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Subtraction and two numbers/subtraction-and-two-numbers.java
@@ -0,0 +1,17 @@
+// User function Template for Java
+
+class Solution {
+    int repeatedSubtraction(int A, int B) {
+        // code here
+        int count=0;
+        while(A>0&&B>0 ){
+            if(A>B){
+                A=A-B;
+                
+            }else{
+                B=B-A;
+                
+                }
+            count++; }return count;
+    }
+}
