4122a51ada9eaf0ad1a9f13eae778936ee89a65d
KKU89 <krishnakantdeewan@gmail.com>
2026-06-26 11:41:59 -0400

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Count Matching Subsequences/count-matching-subsequences.java
@@ -0,0 +1,20 @@
+class Solution {
+    public static int countWays(String s1, String s2) {
+        int n = s1.length();
+        int m = s2.length();
+        int mod = 1000000007;
+
+        int[] dp = new int[m + 1];
+        dp[0] = 1;
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = m; j >= 1; j--) {
+                if (s1.charAt(i - 1) == s2.charAt(j - 1)) {
+                    dp[j] = (dp[j] + dp[j - 1]) % mod;
+                }
+            }
+        }
+
+        return dp[m];
+    }
+}
